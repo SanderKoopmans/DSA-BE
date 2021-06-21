6dfaa18f397b0bdd6e583630e1d1eef7a2c1ea36
Sander <s.koopmans@ecmxperts.nl>
2021-06-21 08:50:52 +0200

    Add comments as program outline

--- src/routes/fizzBuzz.js
+++ src/routes/fizzBuzz.js
@@ -4,6 +4,11 @@ const FizzBuzzRouter = Router({ mergeParams: true });
 
 //TODO: implement '/:number' route to reply according to fizzbuzz rules
 FizzBuzzRouter.post('/:number', (req, res) => {
+  // Check incoming :number if it's a number
+
+  // Check fizzBuzz requirements
+
+  // Send status code + answer
     
 });
 
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -16,6 +16,31 @@ TodoRoute.post('/', function (req,res) {
     addTodo(req.body);
 });
 
+// Validation tests
+// Create a function to invoke a controller for TodoRoute
+// Test controller functionality:
+
+// - test if incoming request is of type POST, else send 405
+
+// - test if Incoming object has the format of {
+//     "type": "object",
+//     "properties": {
+//         "title": { "type": "string" },
+//         "created": { "type": "string", "format": "date-time" }
+//     },
+//     "additionalProperties:": false
+// }
+
+// - test if Object = valid JSON
+
+// - test if properties has a title of type string, without any odd characters only allow 'a-z 0-9 - _'
+
+// - test if properties has a created of type string, without any odd characters only allow 'a-z 0-9 - _'
+// - test if format matches format as specified in the API documentation
+
+// - test if route responds with 400 if any additional properties are added
+
+
 
 
 module.exports = TodoRoute;
