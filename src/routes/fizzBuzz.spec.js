b8abfb625845d7b13b279bfc3fe17e10f7691368
Sander <s.koopmans@ecmxperts.nl>
2021-06-21 09:01:01 +0200

    Setup fizzBuzz test file

---
+++ src/routes/fizzBuzz.spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai')
+  , expect = chai.expect
+  , should = chai.should();
+
+describe('fizzBuzz route', () => {
+  it('fizzBuzz fake test', () => {
+    let test = true;
+    expect(test).equal(true);
+  });
+});
